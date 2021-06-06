52465d4b2e078544dcf8878a0bfe006952a8ca79
jdesmond91 <jonathandesmond91@gmail.com>
2021-06-06 17:52:16 -0400

    moved Funds to its own component

--- src/App.js
+++ src/App.js
@@ -1,60 +1,12 @@
 import './App.css'
-import { useState } from 'react'
 import Header from './components/Header'
 import Portfolio from './components/Portfolio'
 
-const FundsForm = ({ addFunds }) => {
-	const [value, setValue] = useState(0)
-
-	const handleValueChange = (event) => {
-		setValue(parseInt(event.target.value))
-	}
-
-	const handleFundsSubmit = (event) => {
-		event.preventDefault()
-		addFunds(value)
-	}
-
-	return (
-		<div>
-			<form onSubmit={handleFundsSubmit}>
-				<label htmlFor='funds'>Add Funds</label>
-				<input type='number' name='funds' id='funds' onChange={handleValueChange} />
-				<button id='submit' type='submit'>
-					Add Funds
-				</button>
-			</form>
-		</div>
-	)
-}
-
 const App = () => {
-	const [total, setTotal] = useState(0)
-
-	const handleIncrease = () => {
-		setTotal(total + 1)
-	}
-
-	const handleDecrease = () => {
-		setTotal(total - 1)
-	}
-
-	const addFunds = (value) => {
-		setTotal(value)
-	}
-
 	return (
 		<div>
 			<Header />
 			<div className='container'>
-				{/* <FundsForm addFunds={addFunds} />
-				<div className='total'>
-					{total}
-					<div className='totalButtons'>
-						<button onClick={handleIncrease}>Increase</button>
-						<button onClick={handleDecrease}>Decrease</button>
-					</div>
-				</div> */}
 				<Portfolio />
 			</div>
 		</div>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,19 +1,44 @@
 import React, { useState } from 'react'
+import FundsForm from './Funds'
 import Position from './Position'
 import Security from './Security'
 import securityService from '../services/security'
 import transactionService from '../services/transaction'
 
 const Portfolio = () => {
-	const [netWorth, setNetWorth] = useState(5000)
+	const [netWorth, setNetWorth] = useState(0)
+	const [cash, setCash] = useState(0)
 	const [securities, setSecurities] = useState(securityService.initialSecurities)
 	const [transactions, setTransactions] = useState(transactionService.initializeTransactions())
 	const [positions, setPositions] = useState(transactionService.getPositions(transactions))
 
-	//console.log(transactionService.getTransactionTotalValue(transactions))
+	const handleIncrease = () => {
+		setCash(cash + 1)
+	}
+
+	const handleDecrease = () => {
+		setCash(cash - 1)
+	}
+
+	const addFunds = (value) => {
+		setCash(value)
+	}
+
+	const handleNetWorth = () => {
+		const total = cash + transactionService.getTransactionTotalValue
+		setNetWorth(total)
+	}
 
 	return (
 		<>
+			<FundsForm addFunds={addFunds} />
+			<div className='total'>
+				{cash}
+				<div className='totalButtons'>
+					<button onClick={handleIncrease}>Increase</button>
+					<button onClick={handleDecrease}>Decrease</button>
+				</div>
+			</div>
 			<Position securities={securities} positions={positions} />
 			<Security />
 		</>
