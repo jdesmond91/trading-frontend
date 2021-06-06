b144b23f5b4e4d3b475c43c564334e72c2d98796
jdesmond91 <jonathandesmond91@gmail.com>
2021-06-06 15:56:34 -0400

    initial clean up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react'
+import Header from './components/Header'
+import Portfolio from './components/Portfolio'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const FundsForm = ({ addFunds }) => {
+	const [value, setValue] = useState(0)
+
+	const handleValueChange = (event) => {
+		setValue(parseInt(event.target.value))
+	}
+
+	const handleFundsSubmit = (event) => {
+		event.preventDefault()
+		addFunds(value)
+	}
+
+	return (
+		<div>
+			<form onSubmit={handleFundsSubmit}>
+				<label htmlFor='funds'>Add Funds</label>
+				<input type='number' name='funds' id='funds' onChange={handleValueChange} />
+				<button id='submit' type='submit'>
+					Add Funds
+				</button>
+			</form>
+		</div>
+	)
 }
 
-export default App;
+const App = () => {
+	const [total, setTotal] = useState(0)
+
+	const handleIncrease = () => {
+		setTotal(total + 1)
+	}
+
+	const handleDecrease = () => {
+		setTotal(total - 1)
+	}
+
+	const addFunds = (value) => {
+		setTotal(value)
+	}
+
+	return (
+		<div>
+			<Header />
+			<div className='container'>
+				{/* <FundsForm addFunds={addFunds} />
+				<div className='total'>
+					{total}
+					<div className='totalButtons'>
+						<button onClick={handleIncrease}>Increase</button>
+						<button onClick={handleDecrease}>Decrease</button>
+					</div>
+				</div> */}
+				<Portfolio />
+			</div>
+		</div>
+	)
+}
+
+export default App
