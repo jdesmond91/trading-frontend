e6aae6a7a538bbd60517c27992cbe60440353841
jdesmond91 <jonathandesmond91@gmail.com>
2021-06-06 21:58:37 -0400

    added react router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom'
+
 const Header = () => {
 	return (
 		<header>
 			<nav className='nav flex'>
-				<a href=''>Funds</a>
-				<a href=''>Portfolio</a>
+				<Link to='/'>Portfolio</Link>
+				<Link to='/test'>Test</Link>
 			</nav>
 		</header>
 	)
