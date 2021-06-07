76bbf3f06b911c07458ba4d1a0b7c35a6869cc2a
jdesmond91 <jonathandesmond91@gmail.com>
2021-06-06 21:58:37 -0400

    added react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './App.css'
 import Header from './components/Header'
 import Portfolio from './components/Portfolio'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+const Test = () => <div>Hello</div>
 
 const App = () => {
 	return (
-		<div>
-			<Header />
-			<div className='container'>
-				<Portfolio />
+		<Router>
+			<div>
+				<Header />
+				<div className='container'>
+					<Switch>
+						<Route exact path='/'>
+							<Portfolio />
+						</Route>
+						<Route path='/test'>
+							<Test />
+						</Route>
+					</Switch>
+				</div>
 			</div>
-		</div>
+		</Router>
 	)
 }
 
