59462a9faa85ecf17d859e9c229ae9b038b16df4
jdesmond91 <jonathandesmond91@gmail.com>
2021-06-15 19:33:37 -0400

    added action to add transactions to state

--- src/redux/transactionsSlice.js
+++ src/redux/transactionsSlice.js
@@ -10,10 +10,22 @@ export const transactionsSlice = createSlice({
 		setTransactions: (state, action) => {
 			state.value = action.payload
 		},
+		addTransactions: (state, action) => {
+			if (!state.value[action.payload.secId]) {
+				state.value[action.payload.secId] = []
+			}
+
+			state.value[action.payload.secId].push({
+				id: action.payload.id,
+				type: action.payload.type,
+				quantity: action.payload.quantity,
+				price: action.payload.price,
+			})
+		},
 	},
 })
 
 // Action creators are generated for each case reducer function
-export const { setTransactions } = transactionsSlice.actions
+export const { setTransactions, addTransactions } = transactionsSlice.actions
 
 export default transactionsSlice.reducer
