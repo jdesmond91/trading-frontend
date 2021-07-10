99bf764c2ab81b3eae9ec0caf5b06bf8d5cfb4e5
jdesmond91 <jonathandesmond91@gmail.com>
2021-07-10 19:23:58 -0400

    import baseUrl from config

--- src/services/positions.js
+++ src/services/positions.js
@@ -1,27 +1,23 @@
 import axios from 'axios'
-const baseUrl = '/api/positions'
+import { baseUrl } from '../config'
 
 const getPositions = async () => {
-	const res = await axios.get(`http://localhost:3001/api/positions/`)
-	console.log(res.data)
+	const res = await axios.get(`${baseUrl}/positions`)
 	return res.data
 }
 
 const getCash = async () => {
-	const res = await axios.get(`http://localhost:3001/api/positions/cash`)
-	console.log(res.data.quantity)
+	const res = await axios.get(`${baseUrl}/positions/cash`)
 	return res.data.quantity
 }
 
 const depositCash = async (quantity) => {
-	const res = await axios.post(`http://localhost:3001/api/transactions/deposit`, { quantity })
-	console.log(res.data)
+	const res = await axios.post(`${baseUrl}/transactions/deposit`, { quantity })
 	return res.data
 }
 
 const getNetWorth = async () => {
-	const res = await axios.get(`http://localhost:3001/api/positions/networth`)
-	console.log(res.data)
+	const res = await axios.get(`${baseUrl}/positions/networth`)
 	return res.data
 }
 
