c684ab97de669f9ce063e62193be4250b86d346e
jdesmond91 <jonathandesmond91@gmail.com>
2021-07-10 21:37:08 -0400

    initialize as empty array

--- src/redux/transactionsSlice.js
+++ src/redux/transactionsSlice.js
@@ -1,31 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit'
-import transactionService from '../services/transaction'
 
 export const transactionsSlice = createSlice({
 	name: 'transactions',
 	initialState: {
-		value: transactionService.initializeTransactions(),
+		value: [],
 	},
 	reducers: {
 		setTransactions: (state, action) => {
 			state.value = action.payload
 		},
-		addTransactions: (state, action) => {
-			if (!state.value[action.payload.secId]) {
-				state.value[action.payload.secId] = []
-			}
-
-			state.value[action.payload.secId].push({
-				id: action.payload.id,
-				type: action.payload.type,
-				quantity: action.payload.quantity,
-				price: action.payload.price,
-			})
-		},
 	},
 })
 
 // Action creators are generated for each case reducer function
-export const { setTransactions, addTransactions } = transactionsSlice.actions
+export const { setTransactions } = transactionsSlice.actions
 
 export default transactionsSlice.reducer
