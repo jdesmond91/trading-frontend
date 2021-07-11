81c027da23ee23bee096b377e720c20f123485c9
jdesmond91 <jonathandesmond91@gmail.com>
2021-07-10 21:37:53 -0400

    added route to transaction history

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,7 @@ const Header = () => {
 			<nav className='nav flex'>
 				<Link to='/'>Portfolio</Link>
 				<Link to='/order'>Order</Link>
+				<Link to='/transaction'>Transaction History</Link>
 			</nav>
 		</header>
 	)
