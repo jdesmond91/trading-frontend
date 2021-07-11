34022467ca0425498853f9c3573e0ba2ffeefc31
jdesmond91 <jonathandesmond91@gmail.com>
2021-07-10 21:48:05 -0400

    initialize with empty array

--- src/redux/securitiesSlice.js
+++ src/redux/securitiesSlice.js
@@ -1,37 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit'
-import securityService from '../services/security'
-
-const initialSecurities = [
-	{
-		id: 1,
-		name: 'Royal Bank',
-		ticker: 'RY',
-		price: securityService.getIntradayPrice(120),
-	},
-	{
-		id: 2,
-		name: 'Apple',
-		ticker: 'AAPL',
-		price: securityService.getIntradayPrice(200),
-	},
-	{
-		id: 3,
-		name: 'IBM',
-		ticker: 'IBM',
-		price: securityService.getIntradayPrice(75),
-	},
-	{
-		id: 4,
-		name: 'Microsoft',
-		ticker: 'MFST',
-		price: securityService.getIntradayPrice(150),
-	},
-]
 
 export const securitiesSlice = createSlice({
 	name: 'securities',
 	initialState: {
-		value: initialSecurities,
+		value: [],
 	},
 	reducers: {
 		setSecurities: (state, action) => {
