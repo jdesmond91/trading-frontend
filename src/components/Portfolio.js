782c2a041e112939c83fa3259240dd3c8187ed01
jdesmond91 <jonathandesmond91@gmail.com>
2021-07-10 21:47:07 -0400

    removed Order component from Portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,8 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import FundsForm from './Funds'
 import Position from './Position'
-import Order from './Order'
-import transactionService from '../services/transaction'
 import positionService from '../services/positions'
 import { useSelector, useDispatch } from 'react-redux'
 import { setNetWorth } from '../redux/netWorthSlice'
@@ -13,16 +11,8 @@ const Portfolio = () => {
 	// get from redux store
 	const netWorth = useSelector((state) => state.netWorth.value)
 	const cash = useSelector((state) => state.cash.value)
-	const securities = useSelector((state) => state.securities.value)
-	const transactions = useSelector((state) => state.transactions.value)
-	const positions = useSelector((state) => state.positions.value)
 
 	const dispatch = useDispatch()
-
-	const handleNetWorth = () => {
-		const total = cash + transactionService.getTransactionTotalValue(securities, positions)
-		dispatch(setNetWorth(total))
-	}
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -30,7 +20,7 @@ const Portfolio = () => {
 		}
 
 		fetchData()
-	}, [dispatch])
+	}, [dispatch, cash])
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -38,11 +28,7 @@ const Portfolio = () => {
 		}
 
 		fetchData()
-	}, [dispatch])
-
-	// useEffect(() => {
-	// 	dispatch(setPositions(transactionService.getPositions(transactions)))
-	// }, [transactions])
+	}, [dispatch, cash])
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -50,7 +36,7 @@ const Portfolio = () => {
 		}
 
 		fetchData()
-	}, [dispatch])
+	}, [dispatch, cash])
 
 	return (
 		<>
@@ -62,7 +48,6 @@ const Portfolio = () => {
 			</div>
 			<FundsForm />
 			<Position />
-			<Order />
 		</>
 	)
 }
