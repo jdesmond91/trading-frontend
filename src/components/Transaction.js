570ea66511305f7c0a5c6130d0d4a4cd4b3f7a38
jdesmond91 <jonathandesmond91@gmail.com>
2021-07-10 21:38:07 -0400

    created Transaction component

---
+++ src/components/Transaction.js
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import transactionService from '../services/transactions'
+import { setTransactions } from '../redux/transactionsSlice'
+
+const Transaction = () => {
+	const transactions = useSelector((state) => state.transactions.value)
+	const dispatch = useDispatch()
+	useEffect(() => {
+		const fetchData = async () => {
+			dispatch(setTransactions(await transactionService.getTransactions()))
+		}
+
+		fetchData()
+	}, [dispatch])
+
+	return (
+		<table>
+			<thead>
+				<tr>
+					<th>Type</th>
+					<th>Date</th>
+					<th>Quantity</th>
+				</tr>
+			</thead>
+			<tbody>
+				{transactions.map((transaction) => {
+					return (
+						<tr key={transaction.id}>
+							<td>{transaction.type}</td>
+							<td>{transaction.date.split('T')[0]}</td>
+							<td>{transaction.quantity}</td>
+						</tr>
+					)
+				})}
+			</tbody>
+		</table>
+	)
+}
+
+export default Transaction
