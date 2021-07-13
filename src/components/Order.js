f2a4b40548f729f95a5d15db328c59797764d090
jdesmond91 <jonathandesmond91@gmail.com>
2021-07-12 20:19:03 -0400

    updated sell logic to ensure you have enough positions in order to make the sale

--- src/components/Order.js
+++ src/components/Order.js
@@ -11,6 +11,7 @@ import { setPositions } from '../redux/positionsSlice'
 const Order = () => {
 	const securities = useSelector((state) => state.securities.value)
 	const cash = useSelector((state) => state.cash.value)
+	const positions = useSelector((state) => state.positions.value)
 
 	const [selected, setSelected] = useState(null)
 	const [quantity, setQuantity] = useState(1)
@@ -54,22 +55,29 @@ const Order = () => {
 					alert('You do not have enough cash!')
 				}
 			} else {
-				order = {
-					type: 'SELL',
-					securityId: selected.id,
-					quantity: quantity,
+				const position = positions.find((position) => position.security.name === selected.name)
+				if (quantity <= position.quantity) {
+					order = {
+						type: 'SELL',
+						securityId: selected.id,
+						quantity: quantity,
+					}
+				} else {
+					alert('You do not own enough positions to sell!')
 				}
 			}
 
-			try {
-				const newOrder = await orderService.createOrder(order)
-				dispatch(setCash(await positionService.getCash()))
-				dispatch(setPositions(await positionService.getPositions()))
-				dispatch(setTransactions(await transactionService.getTransactions()))
-				console.log('newOrder', newOrder)
-			} catch (err) {
-				alert('Unable to complete order')
-			}
+			console.log(order)
+
+			// try {
+			// 	const newOrder = await orderService.createOrder(order)
+			// 	dispatch(setCash(await positionService.getCash()))
+			// 	dispatch(setPositions(await positionService.getPositions()))
+			// 	dispatch(setTransactions(await transactionService.getTransactions()))
+			// 	console.log('newOrder', newOrder)
+			// } catch (err) {
+			// 	alert('Unable to complete order')
+			// }
 		} else {
 			alert('Please select a security')
 		}
