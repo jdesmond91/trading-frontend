c22991fb9b9f79e0acff6eeaf420c8957faf0a81
jdesmond91 <jonathandesmond91@gmail.com>
2021-07-17 17:30:42 -0400

    added account balance icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ const Header = () => {
 	return (
 		<header>
 			<nav className='nav flex'>
+				<span class='material-icons'>account_balance</span>
 				<Link to='/'>Portfolio</Link>
 				<Link to='/order'>Order</Link>
 				<Link to='/transaction'>Transaction History</Link>
