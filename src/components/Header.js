fcd96561a4048c37e46a7820c399e3206b28ba6c
jdesmond91 <jonathandesmond91@gmail.com>
2021-07-18 16:04:55 -0400

    added shadow to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,12 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
 	return (
-		<header>
+		<header className='shadow'>
 			<nav className='nav flex'>
 				<span class='material-icons'>account_balance</span>
 				<Link to='/'>Portfolio</Link>
 				<Link to='/order'>Order</Link>
-				<Link to='/transaction'>Transaction History</Link>
+				<Link to='/transaction'>Transactions</Link>
 			</nav>
 		</header>
 	)
