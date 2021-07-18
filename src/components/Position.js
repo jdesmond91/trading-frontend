77574761262351b6fc1c216d7d2fc0c6b51b206a
jdesmond91 <jonathandesmond91@gmail.com>
2021-07-18 16:05:43 -0400

    converted from div table to html table

--- src/components/Position.js
+++ src/components/Position.js
@@ -5,23 +5,28 @@ const Position = () => {
 	const positions = useSelector((state) => state.positions.value)
 
 	return (
-		<div className='grid'>
-			<div></div>
-			<div>Quantity</div>
-			<div>Today's Price</div>
-			<div>Total Value</div>
-
-			{positions.map((position) => {
-				return (
-					<React.Fragment key={position.id}>
-						<div>{position.security.name}</div>
-						<div>{position.quantity}</div>
-						<div>{position.security.price}</div>
-						<div>{position.totalValue}</div>
-					</React.Fragment>
-				)
-			})}
-		</div>
+		<table>
+			<thead>
+				<tr>
+					<th></th>
+					<th>Quantity</th>
+					<th>Price</th>
+					<th>Total Value</th>
+				</tr>
+			</thead>
+			<tbody>
+				{positions.map((position) => {
+					return (
+						<tr key={position.id}>
+							<td>{position.security.name}</td>
+							<td>{position.quantity}</td>
+							<td>{position.security.price}</td>
+							<td>{position.totalValue}</td>
+						</tr>
+					)
+				})}
+			</tbody>
+		</table>
 	)
 }
 
