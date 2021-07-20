3882a8c3a4a0fcbf002d0df1d29f6dddaf4fe47b
jdesmond91 <jonathandesmond91@gmail.com>
2021-07-19 21:07:05 -0400

    removed commented useEffects and unused imports

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,42 +1,12 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import FundsForm from './Funds'
 import Position from './Position'
-import positionService from '../services/positions'
-import { useSelector, useDispatch } from 'react-redux'
-import { setNetWorth } from '../redux/netWorthSlice'
-import { setPositions } from '../redux/positionsSlice'
-import { setCash } from '../redux/cashSlice'
+import { useSelector } from 'react-redux'
 
 const Portfolio = () => {
 	// get from redux store
 	const netWorth = useSelector((state) => state.netWorth.value)
 	const cash = useSelector((state) => state.cash.value)
-
-	// const dispatch = useDispatch()
-
-	// useEffect(() => {
-	// 	const fetchData = async () => {
-	// 		dispatch(setCash(await positionService.getCash()))
-	// 	}
-
-	// 	fetchData()
-	// }, [dispatch, cash])
-
-	// useEffect(() => {
-	// 	const fetchData = async () => {
-	// 		dispatch(setNetWorth(await positionService.getNetWorth()))
-	// 	}
-
-	// 	fetchData()
-	// }, [dispatch, cash])
-
-	// useEffect(() => {
-	// 	const fetchData = async () => {
-	// 		dispatch(setPositions(await positionService.getPositions()))
-	// 	}
-
-	// 	fetchData()
-	// }, [dispatch, cash])
 
 	return (
 		<main className='container'>
