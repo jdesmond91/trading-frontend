8f19d6ad549dbab5911aeebeb327b443fa6f90a6
jdesmond91 <jonathandesmond91@gmail.com>
2021-07-20 21:00:08 -0400

    corrected class to className

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
 	return (
 		<header className='shadow'>
 			<nav className='nav'>
-				<span class='material-icons'>account_balance</span>
+				<span className='material-icons'>account_balance</span>
 				<Link to='/'>Portfolio</Link>
 				<Link to='/order'>Order</Link>
 				<Link to='/transaction'>Transactions</Link>
