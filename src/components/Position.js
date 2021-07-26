6a1b132050eb169316e702de5d4373704f501ce9
jdesmond91 <jonathandesmond91@gmail.com>
2021-07-25 20:31:37 -0400

    converted classes to BEM methodology
    block classes:
    section
    table

--- src/components/Position.js
+++ src/components/Position.js
@@ -5,28 +5,30 @@ const Position = () => {
 	const positions = useSelector((state) => state.positions.value)
 
 	return (
-		<table className='background'>
-			<thead>
-				<tr>
-					<th></th>
-					<th>Quantity</th>
-					<th>Price</th>
-					<th>Total Value</th>
-				</tr>
-			</thead>
-			<tbody>
-				{positions.map((position) => {
-					return (
-						<tr key={position.id}>
-							<td>{position.security.name}</td>
-							<td>{position.quantity}</td>
-							<td>{position.security.price}</td>
-							<td>{position.totalValue}</td>
-						</tr>
-					)
-				})}
-			</tbody>
-		</table>
+		<section className='section'>
+			<table className='table'>
+				<thead className='table__head'>
+					<tr className='table__row'>
+						<th className='table__heading'></th>
+						<th className='table__heading'>Quantity</th>
+						<th className='table__heading'>Price</th>
+						<th className='table__heading'>Total Value</th>
+					</tr>
+				</thead>
+				<tbody className='table__body'>
+					{positions.map((position) => {
+						return (
+							<tr className='table__row' key={position.id}>
+								<td className='table__data'>{position.security.name}</td>
+								<td className='table__data'>{position.quantity}</td>
+								<td className='table__data'>{position.security.price}</td>
+								<td className='table__data'>{position.totalValue}</td>
+							</tr>
+						)
+					})}
+				</tbody>
+			</table>
+		</section>
 	)
 }
 
