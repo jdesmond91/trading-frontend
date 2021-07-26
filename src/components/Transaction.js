822e2aae434722e066fe44f49b773ffe60fee89d
jdesmond91 <jonathandesmond91@gmail.com>
2021-07-25 20:32:15 -0400

    converted classes to BEM methodology
    block classes:
    transactions
    article
    grid-container
    section
    table

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -15,28 +15,30 @@ const Transaction = () => {
 	}, [dispatch])
 
 	return (
-		<article className='transactions background'>
-			<h2>Transactions</h2>
-			<table>
-				<thead>
-					<tr>
-						<th>Type</th>
-						<th>Date</th>
-						<th>Quantity</th>
-					</tr>
-				</thead>
-				<tbody>
-					{transactions.map((transaction) => {
-						return (
-							<tr key={transaction.id}>
-								<td>{transaction.type}</td>
-								<td>{transaction.date.split('T')[0]}</td>
-								<td>{transaction.quantity}</td>
-							</tr>
-						)
-					})}
-				</tbody>
-			</table>
+		<article className='transactions article grid-container'>
+			<section className='section flex-wrapper'>
+				<h2 className='section__heading'>Transactions</h2>
+				<table className='table'>
+					<thead className='table__head'>
+						<tr className='table__row'>
+							<th className='table__heading'>Type</th>
+							<th className='table__heading'>Date</th>
+							<th className='table__heading'>Quantity</th>
+						</tr>
+					</thead>
+					<tbody className='table__body'>
+						{transactions.map((transaction) => {
+							return (
+								<tr className='table__row' key={transaction.id}>
+									<td className='table__data'>{transaction.type}</td>
+									<td className='table__data'>{transaction.date.split('T')[0]}</td>
+									<td className='table__data'>{transaction.quantity}</td>
+								</tr>
+							)
+						})}
+					</tbody>
+				</table>
+			</section>
 		</article>
 	)
 }
