19a7892086eafbb1f1c7a9041864b7fb29422b5e
jdesmond91 <jonathandesmond91@gmail.com>
2021-07-25 20:28:50 -0400

    converted classes to BEM methodology
    block classes: header and nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,18 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
 	return (
-		<header className='shadow'>
+		<header className='header header--shadow'>
 			<nav className='nav'>
-				<span className='material-icons'>account_balance</span>
-				<Link to='/'>Portfolio</Link>
-				<Link to='/order'>Order</Link>
-				<Link to='/transaction'>Transactions</Link>
+				<span className='nav__item material-icons'>account_balance</span>
+				<Link className='nav__item' to='/'>
+					Portfolio
+				</Link>
+				<Link className='nav__item' to='/order'>
+					Order
+				</Link>
+				<Link className='nav__item' to='/transaction'>
+					Transactions
+				</Link>
 			</nav>
 		</header>
 	)
