3a7ca0ca06b0dff92483f62e33bbbef61a40c6ac
jdesmond91 <jonathandesmond91@gmail.com>
2021-07-27 17:12:27 -0400

    Moved positionService useEffects back to Portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import FundsForm from './Funds'
 import Position from './Position'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import positionService from '../services/positions'
+import { setNetWorth } from '../redux/netWorthSlice'
+import { setPositions } from '../redux/positionsSlice'
+import { setCash } from '../redux/cashSlice'
 
 const Portfolio = () => {
 	// get from redux store
 	const netWorth = useSelector((state) => state.netWorth.value)
 	const cash = useSelector((state) => state.cash.value)
+
+	const dispatch = useDispatch()
+
+	useEffect(() => {
+		const fetchData = async () => {
+			dispatch(setCash(await positionService.getCash()))
+		}
+
+		fetchData()
+	}, [dispatch])
+
+	useEffect(() => {
+		const fetchData = async () => {
+			dispatch(setNetWorth(await positionService.getNetWorth()))
+		}
+
+		fetchData()
+	}, [dispatch])
+
+	useEffect(() => {
+		const fetchData = async () => {
+			dispatch(setPositions(await positionService.getPositions()))
+		}
+
+		fetchData()
+	}, [dispatch])
 
 	return (
 		<article className='portfolio article grid-container'>
