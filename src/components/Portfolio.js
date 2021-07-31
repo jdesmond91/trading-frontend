092fb7d160caf5041615eab3a8a3b95573d0ee8a
jdesmond91 <jonathandesmond91@gmail.com>
2021-07-30 23:20:58 -0400

    moved useEffects to App and added data-cy tags for easier selection in cypress tests

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,49 +1,21 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import FundsForm from './Funds'
 import Position from './Position'
-import { useSelector, useDispatch } from 'react-redux'
-import positionService from '../services/positions'
-import { setNetWorth } from '../redux/netWorthSlice'
-import { setPositions } from '../redux/positionsSlice'
-import { setCash } from '../redux/cashSlice'
+import { useSelector } from 'react-redux'
 
 const Portfolio = () => {
 	// get from redux store
 	const netWorth = useSelector((state) => state.netWorth.value)
 	const cash = useSelector((state) => state.cash.value)
 
-	const dispatch = useDispatch()
-
-	useEffect(() => {
-		const fetchData = async () => {
-			dispatch(setCash(await positionService.getCash()))
-		}
-
-		fetchData()
-	}, [dispatch])
-
-	useEffect(() => {
-		const fetchData = async () => {
-			dispatch(setNetWorth(await positionService.getNetWorth()))
-		}
-
-		fetchData()
-	}, [dispatch])
-
-	useEffect(() => {
-		const fetchData = async () => {
-			dispatch(setPositions(await positionService.getPositions()))
-		}
-
-		fetchData()
-	}, [dispatch])
-
 	return (
 		<article className='portfolio article grid-container'>
 			<section className='net-worth section section--no-background'>
 				<h1 className='section__heading'>Total Net Worth</h1>
 				<hr className='section__hr' />
-				<p className='section__main'>${netWorth}</p>
+				<p className='section__main' data-cy='net-worth'>
+					${netWorth}
+				</p>
 			</section>
 
 			<section className='funds section'>
