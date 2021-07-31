915385759ed9f69aac210e398a7e4e04cd2ce83d
jdesmond91 <jonathandesmond91@gmail.com>
2021-07-30 23:19:02 -0400

    moved useEffects to App to accommodate cypress tests

--- src/App.js
+++ src/App.js
@@ -7,8 +7,12 @@ import Transaction from './components/Transaction'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import ScrollToTop from './components/ScrollToTop'
 import securityService from './services/securities'
+import positionService from './services/positions'
 import { useDispatch } from 'react-redux'
 import { setSecurities } from './redux/securitiesSlice'
+import { setNetWorth } from './redux/netWorthSlice'
+import { setPositions } from './redux/positionsSlice'
+import { setCash } from './redux/cashSlice'
 
 const App = () => {
 	const dispatch = useDispatch()
@@ -16,6 +20,30 @@ const App = () => {
 	useEffect(() => {
 		const fetchData = async () => {
 			dispatch(setSecurities(await securityService.getSecurities()))
+		}
+
+		fetchData()
+	}, [dispatch])
+
+	useEffect(() => {
+		const fetchData = async () => {
+			dispatch(setCash(await positionService.getCash()))
+		}
+
+		fetchData()
+	}, [dispatch])
+
+	useEffect(() => {
+		const fetchData = async () => {
+			dispatch(setNetWorth(await positionService.getNetWorth()))
+		}
+
+		fetchData()
+	}, [dispatch])
+
+	useEffect(() => {
+		const fetchData = async () => {
+			dispatch(setPositions(await positionService.getPositions()))
 		}
 
 		fetchData()
