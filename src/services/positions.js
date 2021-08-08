730bd75e873d39d3af2d65c9fb36feec223c5a38
jdesmond91 <jonathandesmond91@gmail.com>
2021-08-08 19:24:58 -0400

    updated getPositions method to use new endpoint

--- src/services/positions.js
+++ src/services/positions.js
@@ -2,13 +2,13 @@ import axios from 'axios'
 import baseUrl from '../config'
 
 const getPositions = async () => {
-	const res = await axios.get(`${baseUrl}/positions`)
+	const res = await axios.get(`${baseUrl}/positions/equity`)
 	return res.data
 }
 
 const getCash = async () => {
 	const res = await axios.get(`${baseUrl}/positions/cash`)
-	return res.data.quantity
+	return res.data
 }
 
 const depositCash = async (quantity) => {
