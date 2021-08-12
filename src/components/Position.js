78f883c3d3115a93bc1479810c203ace059c92b1
jdesmond91 <jonathandesmond91@gmail.com>
2021-08-11 22:59:17 -0400

    Reworked Position component to display quantity as well as performance
    Removed table headings
    
    Position row data will now give you more information on the current market value of your position. As well as its current performance

--- src/components/Position.js
+++ src/components/Position.js
@@ -1,31 +1,42 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import round from '../utils/round'
 
 const Position = () => {
 	const positions = useSelector((state) => state.positions.value)
 
 	return (
 		<section className='section'>
+			<h2 className='section__heading'>Portfolio</h2>
+			<hr className='section__hr' />
 			<table className='table'>
-				<thead className='table__head'>
-					<tr className='table__row'>
-						<th className='table__heading'></th>
-						<th className='table__heading'>Quantity</th>
-						<th className='table__heading'>Price</th>
-						<th className='table__heading'>Total Value</th>
-					</tr>
-				</thead>
 				<tbody className='table__body'>
-					{positions.map((position) => {
-						return (
-							<tr className='table__row' key={position.id}>
-								<td className='table__data'>{position.security.name}</td>
-								<td className='table__data'>{position.quantity}</td>
-								<td className='table__data'>{position.security.price}</td>
-								<td className='table__data'>{position.totalValue}</td>
-							</tr>
-						)
-					})}
+					{positions &&
+						positions.map(({ id, security, quantity, bookValue, marketValue }) => {
+							const difference = round(marketValue - bookValue)
+							const performance = round((marketValue + bookValue) / bookValue)
+							// if there is a net profit, then add a positive sign to the string
+							const differenceString = (difference) => {
+								return difference > 0
+									? `+${difference} (${performance}%)`
+									: `${difference} (${performance}%)`
+							}
+
+							return (
+								<tr className='table__row' key={id}>
+									<td className='table__data'>
+										<div className='table__inner'>
+											<p className='table__detail'>{security.ticker}</p>
+											<p className='table__detail'>{marketValue}</p>
+										</div>
+										<div className='table__inner'>
+											<p className='table__sub-detail'>{quantity} shares</p>
+											<p className='table__sub-detail'>{differenceString(difference)}</p>
+										</div>
+									</td>
+								</tr>
+							)
+						})}
 				</tbody>
 			</table>
 		</section>
