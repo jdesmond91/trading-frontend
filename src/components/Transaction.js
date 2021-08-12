eeeefd81641ea25186064a3a5b1580deff459800
jdesmond91 <jonathandesmond91@gmail.com>
2021-08-11 22:57:49 -0400

    Updated Transaction component row data
    Removed table headings
    
    Transaction row data will now display further information on the security and total value if it is a BUY or SELL.

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -2,6 +2,39 @@ import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import transactionService from '../services/transactions'
 import { setTransactions } from '../redux/transactionsSlice'
+import round from '../utils/round'
+
+const TransactionData = ({ transaction }) => {
+	if (transaction.type === 'DEPOSIT') {
+		return (
+			<td className='table__data'>
+				<div className='table__inner'>
+					<p className='table__detail'>{transaction.type}</p>
+					<p className='table__detail'>{transaction.quantity}</p>
+				</div>
+				<div className='table__inner'>
+					<p className='table__sub-detail'>{transaction.date.split('T')[0]}</p>
+				</div>
+			</td>
+		)
+	} else {
+		// show ticker and total purchase price for BUYS and SELLS
+		const total = round(transaction.quantity * transaction.price)
+		return (
+			<td className='table__data'>
+				<div className='table__inner'>
+					<p className='table__detail'>
+						{transaction.order.security.ticker} <span>{transaction.type}</span>
+					</p>
+					<p className='table__detail'>{total}</p>
+				</div>
+				<div className='table__inner'>
+					<p className='table__sub-detail'>{transaction.date.split('T')[0]}</p>
+				</div>
+			</td>
+		)
+	}
+}
 
 const Transaction = () => {
 	const transactions = useSelector((state) => state.transactions.value)
@@ -20,23 +53,15 @@ const Transaction = () => {
 				<h2 className='section__heading'>Transactions</h2>
 				<hr className='section__hr' />
 				<table className='table'>
-					<thead className='table__head'>
-						<tr className='table__row'>
-							<th className='table__heading'>Type</th>
-							<th className='table__heading'>Date</th>
-							<th className='table__heading'>Quantity</th>
-						</tr>
-					</thead>
 					<tbody className='table__body'>
-						{transactions.map((transaction) => {
-							return (
-								<tr className='table__row' key={transaction.id}>
-									<td className='table__data'>{transaction.type}</td>
-									<td className='table__data'>{transaction.date.split('T')[0]}</td>
-									<td className='table__data'>{transaction.quantity}</td>
-								</tr>
-							)
-						})}
+						{transactions &&
+							transactions.map((transaction) => {
+								return (
+									<tr className='table__row' key={transaction.id}>
+										{<TransactionData transaction={transaction} />}
+									</tr>
+								)
+							})}
 					</tbody>
 				</table>
 			</section>
